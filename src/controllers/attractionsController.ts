29678c4c027d4c130415d822910f40b42b18dd83
Lucas Gonzalez <lucasggonzalez94@outlook.com>
2024-09-13 17:18:57 -0300

    refactor: modularizacion validators attractions

--- src/controllers/attractionsController.ts
+++ src/controllers/attractionsController.ts
@@ -7,6 +7,7 @@ import streamifier from "streamifier";
 import { body, validationResult } from "express-validator";
 
 import prisma from "../prismaClient";
+import { createAttractionValidator } from "../validators/attractions";
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
@@ -19,72 +20,7 @@ const upload = multer({ storage });
 
 export const createAttraction = [
   upload.array("images"),
-  body("title").trim().notEmpty().withMessage("Title is required"),
-  body("description").notEmpty().withMessage("Description is required"),
-  body("location").notEmpty().withMessage("Location is required"),
-  body("category").notEmpty().withMessage("Category is required"),
-  body("recomended")
-    .optional()
-    .isString()
-    .withMessage("Recomended must be a string"),
-  body("services")
-    .optional()
-    .custom((value) => {
-      if (typeof value === "string") {
-        try {
-          const parsedValue = JSON.parse(value);
-          if (Array.isArray(parsedValue)) {
-            return true;
-          }
-          throw new Error("Services must be an array");
-        } catch (err) {
-          throw new Error("Services must be a valid JSON array");
-        }
-      }
-      return Array.isArray(value);
-    })
-    .withMessage("Services must be an array"),
-  body("contactNumber")
-    .optional()
-    .isString()
-    .withMessage("Contact number must be a string"),
-  body("email").optional().isEmail().withMessage("Please enter a valid email"),
-  body("webSite").optional().isURL().withMessage("Please enter a valid URL"),
-  body("instagram")
-    .optional()
-    .isString()
-    .withMessage("Instagram must be a string"),
-  body("facebook")
-    .optional()
-    .isString()
-    .withMessage("Facebook must be a string"),
-  body("timeOpen")
-    .optional()
-    .isString()
-    .withMessage("Time Open must be a string"),
-  body("timeClose")
-    .optional()
-    .isString()
-    .withMessage("Time Close must be a string"),
-  body("duration")
-    .optional()
-    .isString()
-    .withMessage("Duration must be a string"),
-  body("minAge")
-    .toInt()
-    .optional()
-    .isInt({ min: 0 })
-    .withMessage("Min age must be a valid number"),
-  body("maxPersons")
-    .toInt()
-    .optional()
-    .isInt({ min: 1 })
-    .withMessage("Max persons must be a valid number"),
-  body("price")
-    .toFloat()
-    .optional()
-    .isFloat({ min: 0 })
-    .withMessage("Price must be a valid number"),
+  ...createAttractionValidator,
   async (req: Request, res: Response) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
@@ -208,59 +144,7 @@ export const listAttraction = async (req: Request, res: Response) => {
 
 export const editAttraction = [
   upload.array("images"),
-  body("title").trim().notEmpty().withMessage("Title is required"),
-  body("description").notEmpty().withMessage("Description is required"),
-  body("location").notEmpty().withMessage("Location is required"),
-  body("category").notEmpty().withMessage("Category is required"),
-  body("recomended")
-    .optional()
-    .isString()
-    .withMessage("Recomended must be a string"),
-  body("services")
-    .optional()
-    .isArray()
-    .withMessage("Services must be an array"),
-  body("contactNumber")
-    .optional()
-    .isString()
-    .withMessage("Contact number must be a string"),
-  body("email").optional().isEmail().withMessage("Please enter a valid email"),
-  body("webSite").optional().isURL().withMessage("Please enter a valid URL"),
-  body("instagram")
-    .optional()
-    .isString()
-    .withMessage("Instagram must be a string"),
-  body("facebook")
-    .optional()
-    .isString()
-    .withMessage("Facebook must be a string"),
-  body("timeOpen")
-    .optional()
-    .isString()
-    .withMessage("Time Open must be a string"),
-  body("timeClose")
-    .optional()
-    .isString()
-    .withMessage("Time Close must be a string"),
-  body("duration")
-    .optional()
-    .isString()
-    .withMessage("Duration must be a string"),
-  body("minAge")
-    .toInt()
-    .optional()
-    .isInt({ min: 0 })
-    .withMessage("Min age must be a valid number"),
-  body("maxPersons")
-    .toInt()
-    .optional()
-    .isInt({ min: 1 })
-    .withMessage("Max persons must be a valid number"),
-  body("price")
-    .toFloat()
-    .optional()
-    .isInt({ min: 0 })
-    .withMessage("Price must be a valid number"),
+  ...createAttractionValidator,
   async (req: Request, res: Response) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
@@ -275,7 +159,6 @@ export const editAttraction = [
       location,
       category,
       recomended,
-      services,
       contactNumber,
       email,
       webSite,
@@ -288,6 +171,11 @@ export const editAttraction = [
       maxPersons,
       price,
     } = req.body;
+
+    let services = [];
+    if (req.body.services) {
+      services = JSON.parse(req.body.services);
+    }
 
     try {
       const existingAttraction = await prisma.attraction.findUnique({
