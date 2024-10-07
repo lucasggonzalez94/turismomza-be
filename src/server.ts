2cb1808cb935372e5eeded55b3cd71ef9172d0c8
Lucas Gonzalez <lucasggonzalez94@outlook.com>
2024-10-07 16:31:36 -0300

    feat: cors

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import http from "http";
 import { Server } from "socket.io";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 import advertisementsRoutes from "./routes/advertisementsRoutes";
 import authRoutes from "./routes/authRoutes";
@@ -18,6 +19,23 @@ const app: Application = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
+const allowedOrigins = ["http://localhost:3000"];
+
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.indexOf(origin) === -1) {
+        const msg = "El CORS policy no permite acceso desde este origen.";
+        return callback(new Error(msg), false);
+      }
+
+      return callback(null, true);
+    },
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 
