10a67f5d14993ee1bfdb2fcba808acefb8f8642f
Lucas Gonzalez <lucasggonzalez94@outlook.com>
2025-02-19 17:47:03 -0300

    feat: optimizacion de imagenes

--- src/infrastructure/services/CloudinaryService.ts
+++ src/infrastructure/services/CloudinaryService.ts
@@ -13,10 +13,14 @@ export class CloudinaryService {
   ): Promise<{ url: string; publicId: string } | null> {
     return new Promise((resolve, reject) => {
       const uploadStream = cloudinary.v2.uploader.upload_stream(
-        { resource_type: "image" },
+        { resource_type: "image", width: 1600, height: 1600, crop: "limit" },
         (error, result) => {
           if (error || !result) return reject("Cloudinary upload error");
-          resolve({ url: result.secure_url, publicId: result.public_id });
+          const optimizedUrl = result.secure_url.replace(
+            "/upload/",
+            "/upload/f_webp,q_auto/"
+          );
+          resolve({ url: optimizedUrl, publicId: result.public_id });
         }
       );
 
