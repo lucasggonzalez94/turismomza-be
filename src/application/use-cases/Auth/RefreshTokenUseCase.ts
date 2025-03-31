4b4feb2a7a8da439fd0fe1895299a78b3755d555
Lucas Gonzalez <lucasggonzalez94@outlook.com>
2025-03-31 14:45:46 -0300

    feat: validar refresh token

--- src/application/use-cases/Auth/RefreshTokenUseCase.ts
+++ src/application/use-cases/Auth/RefreshTokenUseCase.ts
@@ -7,8 +7,11 @@ export class RefreshTokenUseCase {
   async execute(refreshToken: string) {
     if (!refreshToken) throw new Error("No refresh token provided");
 
+    const isValid = JwtService.verifyRefreshToken(refreshToken);
+    if (!isValid) throw new Error("Invalid refresh token");
+
     const user = await this.refreshTokenRepository.findByToken(refreshToken);
-    if (!user) throw new Error("Invalid refresh token");
+    if (!user) throw new Error("Refresh token not found");
 
     const { accessToken, refreshToken: newRefreshToken } = JwtService.generateTokens(user.userId, user.userRole);
 
